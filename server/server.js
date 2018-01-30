13a544bff5f3557e9a3aea5c1340b1501e566df2
Piervalerio Vignola <Piervalerio@MacBook-Air-di-Piervalerio.local>
2018-01-30 18:38:12 -0500

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -16,6 +16,26 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    // socket.emit('newEmail', {
+    //     from: 'pierva@example.com',
+    //     text: 'Hey, what is happeningh',
+    //     createdAt: 123
+    // });
+
+    socket.emit('newMessage', {
+        from: 'Piervalerio',
+        text: "Hello",
+        createAt: new Date().getTime()
+    });
+
+    // socket.on('createEmail', (newEmail) => {
+    //     console.log('createEmail', newEmail);
+    // });
+
+    socket.on('createMessage', (newMessage) => {
+        console.log('Received new message', newMessage);
+    });
+
     socket.on('disconnect', () => {
         console.log('User disconnected from server');
     })
